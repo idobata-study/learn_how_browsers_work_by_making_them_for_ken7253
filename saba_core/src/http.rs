91ff180eb54357376efe529a8be1e0cbc41782ad
ken7253 <ken7253@gmail.com>
2025-01-22 20:13:59 +0900

    値を構造化する

--- saba_core/src/http.rs
+++ saba_core/src/http.rs
@@ -1,6 +1,6 @@
 use crate::error::Error;
 use alloc::format;
-use alloc::string::String;
+use alloc::string::{String, ToString};
 use alloc::vec::Vec;
 
 #[derive(Debug, Clone)]
@@ -12,6 +12,15 @@ pub struct HttpResponse {
     body: String,
 }
 
+/// HTTP/1.1 の場合のレスポンスサンプル
+/// ---
+/// HTTP/1.1 200 OK
+/// content-encoding: gzip
+///
+/// <!DOCTYPE html>
+/// <html lang="ja">
+/// ...
+/// </html>
 impl HttpResponse {
     pub fn new(raw_response: String) -> Result<Self, Error> {
         let preprocessed_response = raw_response.trim_start().replace("\n\r", "\n");
@@ -25,6 +34,63 @@ impl HttpResponse {
                 )));
             }
         };
+
+        let (headers, body) = match remaining.split_once("\n\n") {
+            Some((h, b)) => {
+                let mut headers = Vec::new();
+                for header in h.split('\n') {
+                    let splitted_header: Vec<&str> = header.splitn(2, ':').collect();
+                    headers.push(Header::new(
+                        String::from(splitted_header[0].trim()),
+                        String::from(splitted_header[1].trim()),
+                    ))
+                }
+                (headers, b)
+            }
+            None => (Vec::new(), remaining),
+        };
+
+        let statues: Vec<&str> = status_line.split(' ').collect();
+
+        Ok(Self {
+            version: statues[0].to_string(),
+            // ステータスコードはu32に変換して、変換できない場合は404を設定する。
+            status_code: statues[1].parse().unwrap_or(404),
+            reason: statues[2].to_string(),
+            headers,
+            body: body.to_string(),
+        })
+    }
+
+    pub fn version(&self) -> String {
+        self.version.clone()
+    }
+
+    pub fn status_code(&self) -> u32 {
+        self.status_code.clone()
+    }
+
+    pub fn reason(&self) -> String {
+        self.reason.clone()
+    }
+
+    pub fn headers(&self) -> Vec<Header> {
+        self.headers.clone()
+    }
+
+    pub fn body(&self) -> String {
+        self.body.clone()
+    }
+
+    /// headersの中からプロパティを指定して取得をする。
+    pub fn header_value(&self, name: &str) -> Result<String, String> {
+        for h in &self.headers {
+            if h.name == name {
+                return Ok(h.value.clone());
+            }
+        }
+
+        Err(format!("failed to find {} in headers", name))
     }
 }
 
