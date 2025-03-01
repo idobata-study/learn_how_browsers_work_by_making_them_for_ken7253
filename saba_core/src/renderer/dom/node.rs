605386b87e97bbf29b4986ab91fc1bbc5b3a5947
ken7253 <ken7253@gmail.com>
2025-03-02 02:27:29 +0900

    Elementの構築まで

--- saba_core/src/renderer/dom/node.rs
+++ saba_core/src/renderer/dom/node.rs
@@ -1,7 +1,12 @@
+use alloc::format;
 use alloc::rc::Rc;
 use alloc::rc::Weak;
 use alloc::string::String;
+use alloc::vec::Vec;
 use core::cell::RefCell;
+use core::str::FromStr;
+
+use crate::renderer::html::attribute::Attribute;
 
 #[derive(Debug, Clone)]
 pub struct Node {
@@ -11,17 +16,29 @@ pub struct Node {
     first_child: Option<Rc<RefCell<Node>>>,
     last_child: Weak<RefCell<Node>>,
     previous_sibling: Weak<RefCell<Node>>,
-    next_sibling: Weak<RefCell<Node>>,
+    next_sibling: Option<Rc<RefCell<Node>>>,
 }
 
 impl Node {
+    pub fn new(kind: NodeKind) -> Self {
+        Self {
+            kind,
+            window: Weak::new(),
+            parent: Weak::new(),
+            first_child: None,
+            last_child: Weak::new(),
+            previous_sibling: Weak::new(),
+            next_sibling: None,
+        }
+    }
+
     pub fn set_parent(&mut self, parent: Weak<RefCell<Node>>) {
         self.parent = parent;
     }
     pub fn parent(&self) -> Weak<RefCell<Node>> {
         self.parent.clone()
     }
-    pub fn set_first_child(&mut self, first_child: Weak<RefCell<Node>>) {
+    pub fn set_first_child(&mut self, first_child: Option<Rc<RefCell<Node>>>) {
         self.first_child = first_child
     }
     pub fn first_child(&self) -> Option<Rc<RefCell<Node>>> {
@@ -30,7 +47,7 @@ impl Node {
     pub fn set_last_child(&mut self, last_child: Weak<RefCell<Node>>) {
         self.last_child = last_child
     }
-    pub fn last_child(&self, last_child: Weak<RefCell<Node>>) -> Weak<RefCell<Node>> {
+    pub fn last_child(&self) -> Weak<RefCell<Node>> {
         self.last_child.clone()
     }
     pub fn set_previous_sibling(&mut self, previous_sibling: Weak<RefCell<Node>>) {
@@ -45,6 +62,10 @@ impl Node {
     pub fn next_sibling(&self) -> Option<Rc<RefCell<Node>>> {
         self.next_sibling.as_ref().cloned()
     }
+
+    pub fn set_window(&mut self, window: Weak<RefCell<Window>>) {
+        self.window = window;
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -52,4 +73,68 @@ pub enum NodeKind {
     Document,
     Element(Element),
     Text(String),
+}
+
+#[derive(Debug, Clone)]
+pub struct Window {
+    document: Rc<RefCell<Node>>,
+}
+
+impl Window {
+    pub fn new() -> Self {
+        let window = Self {
+            document: Rc::new(RefCell::new(Node::new(NodeKind::Document))),
+        };
+
+        window
+            .document
+            .borrow_mut()
+            .set_window(Rc::downgrade(&Rc::new(RefCell::new(window.clone()))));
+
+        window
+    }
+
+    pub fn document(&self) -> Rc<RefCell<Node>> {
+        self.document.clone()
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum ElementKind {
+    Html,
+    Head,
+    Style,
+    Script,
+    Body,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Element {
+    kind: ElementKind,
+    attributes: Vec<Attribute>,
+}
+
+impl Element {
+    pub fn new(element_name: &str, attribute: Vec<Attribute>) -> Self {
+        Self {
+            kind: ElementKind::from_str(element_name)
+                .expect("failed to convert string to ElementKind"),
+            attributes: attribute,
+        }
+    }
+}
+
+impl FromStr for ElementKind {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "html" => Ok(ElementKind::Html),
+            "haed" => Ok(ElementKind::Head),
+            "style" => Ok(ElementKind::Style),
+            "script" => Ok(ElementKind::Script),
+            "body" => Ok(ElementKind::Body),
+            _ => Err(format!("unimplemented element name {:?}", s)),
+        }
+    }
 }
